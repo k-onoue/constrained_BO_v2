13f05fb65ab9f8301669530ae276f61664fbb528
k-onoue <k.onoue0724@gmail.com>
2024-10-12 10:40:25 +0900

    test samplers

--- notebooks/_src.py
+++ notebooks/_src.py
@@ -9,23 +9,13 @@ PROJECT_DIR = config["paths"]["project_dir"]
 LOG_DIR = config["paths"]["logs_dir"]
 sys.path.append(PROJECT_DIR)
 
-from src.utils_warcraft import (
-    judge_location_validity,
-    get_d_to,
-    navigate_through_matrix,
-    manhattan_distance,
-    get_opposite,
-    get_next_coordinate,
-    judge_continuity,
-)
+from src.objectives.warcraft import WarcraftObjective
+from src.samplers.custom import CustomQMCSampler, CustomRandomSampler
+from src.samplers.parafac import ParafacSampler
 
 __all__ = [
-    "WarcraftObjectiveBoTorch",
-    "get_opposite",
-    "judge_continuity",
-    "get_next_coordinate",
-    "judge_location_validity",
-    "get_d_to",
-    "navigate_through_matrix",
-    "manhattan_distance",
+    "WarcraftObjective",
+    "CustomQMCSampler",
+    "CustomRandomSampler",
+    "ParafacSampler",
 ]
